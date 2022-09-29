64fd0ba48172ecaa14879f736657cca6e13f1745
Paulo Reis <paulooliveirv@gmail.com>
2022-09-29 10:14:08 -0300

    adicionando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,13 +6,45 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
 
+        Livro livro1 = new Livro( "DAHMR", 300);
 
-        System.out.println(gato);
+        System.out.println(livro1.nome);
 
 
 /*        int a =5;
         int b = 3;
 
         System.out.println("Hello " + (a+b));*/
+    }
+
+    static class Livro {
+        private String nome;
+        private Integer numPag;
+
+        public Livro(String nome, Integer numPag) {
+            this.nome = nome;
+            this.numPag = numPag;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPag() {
+            return numPag;
+        }
+
+        public void setNumPag(Integer numPag) {
+            this.numPag = numPag;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "PrimeiroPrograma{}";
     }
 }
